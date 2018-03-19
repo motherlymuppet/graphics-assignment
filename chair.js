32582f59bbf5c6d06dc0c018c65561ad688f626d
Steven Lowes <motherlymuppet@users.noreply.github.com>
2018-03-19 17:23:12 +0000

    lots of improvmenets

--- chair.js
+++ chair.js
@@ -16,6 +16,7 @@ var color_wall = [186,252,217,1]
 var color_floor = [190,204,226,1]
 var color_ceiling = [183,177,150,1]
 var color_glass = [0,100,255,0.5]
+var color_gold = [226, 213, 111,1]
 
 var ANGLE_STEP = 0.08
 var STEP_AMOUNT = 0.6
@@ -25,7 +26,19 @@ var eyePos = new THREE.Vector3(0,height,-10)
 var eyeDir = new THREE.Vector3(0, 0, STEP_AMOUNT)
 
 var eyeUp = new THREE.Vector3(0,1,0)
-var eyeSide = new THREE.Vector3(1,0,0)
+var eyeSide = new THREE.Vector3(-1,0,0)
+
+var lampRotation = 0
+
+var gl
+var u_ModelMatrix
+var u_NormalMatrix
+u_LightColor1
+u_LightColor2
+u_LightColor3
+u_LightDirection1
+u_LightDirection2
+u_LightPosition
 
 function main() {
 	// Retrieve <canvas> element
@@ -33,9 +46,12 @@ function main() {
 
 	canvas.width = document.body.clientWidth
 	canvas.height = document.body.clientHeight
+	
+	document.getElementById('lift').value = 0
+	document.getElementById('spin').checked = false
 
 	// Get the rendering context for WebGL
-	var gl = getWebGLContext(canvas);
+	gl = getWebGLContext(canvas);
 	if (!gl) {
 		console.log('Failed to get the rendering context for WebGL');
 		return;
@@ -55,31 +71,16 @@ function main() {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	// Get the storage locations of uniform attributes
-	var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-	var u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix');
+	u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+	u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix');
 	var u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
+	
 	var u_LightColor1 = gl.getUniformLocation(gl.program, 'u_LightColor1');
 	var u_LightColor2 = gl.getUniformLocation(gl.program, 'u_LightColor2');
+	var u_LightColor3 = gl.getUniformLocation(gl.program, 'u_LightColor3');
 	var u_LightDirection1 = gl.getUniformLocation(gl.program, 'u_LightDirection1');
 	var u_LightDirection2 = gl.getUniformLocation(gl.program, 'u_LightDirection2');
-
-	// Trigger using lighting or not
-	var u_isLighting = gl.getUniformLocation(gl.program, 'u_isLighting');
-
-	if (!(u_ModelMatrix && u_NormalMatrix && u_ProjMatrix && u_LightColor1 && u_LightColor2 && u_LightDirection1 && u_LightDirection2 && u_isLighting)) {
-		console.log('Failed to Get the storage locations of matrix');
-		return;
-	}
-
-	// Set the light color (white)
-	gl.uniform3f(u_LightColor1, 1, 1, 1);
-	// Set the light direction (in the world coordinate)
-	var lightDirection1 = new Vector3([0.7, 0.6, -0.2]);
-	gl.uniform3fv(u_LightDirection1, lightDirection1.elements);
-	
-	gl.uniform3f(u_LightColor2, 1, 1, 1);
-	var lightDirection2 = new Vector3([-0.2, -0.4, 0.7]);
-	gl.uniform3fv(u_LightDirection2, lightDirection2.elements);
+	var u_LightPosition = gl.getUniformLocation(gl.program, 'u_LightPosition');
 
 	updateViewMatrix(gl)
 
@@ -89,10 +90,10 @@ function main() {
 	gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
 
 	document.onkeydown = function(ev) {
-		keydown(ev, gl, u_ModelMatrix, u_NormalMatrix, u_isLighting);
+		keydown(ev, );
 	};
 
-	draw(gl, u_ModelMatrix, u_NormalMatrix, u_isLighting);
+	draw(gl, u_ModelMatrix, u_NormalMatrix);
 }
 
 function updateViewMatrix(gl){
@@ -106,7 +107,7 @@ function updateViewMatrix(gl){
 	gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
 }
 
-function keydown(ev, gl, u_ModelMatrix, u_NormalMatrix, u_isLighting) {
+function keydown(ev) {
 	switch (ev.keyCode) {
 		case 40: // Up arrow key
 			eyeDir.applyAxisAngle(eyeSide, -ANGLE_STEP)
@@ -150,7 +151,7 @@ function keydown(ev, gl, u_ModelMatrix, u_NormalMatrix, u_isLighting) {
 	}
 
 	// Draw the scene
-	draw(gl, u_ModelMatrix, u_NormalMatrix, u_isLighting);
+	draw();
 }
 
 function initVertexBuffers(gl) {
@@ -261,13 +262,26 @@ function popMatrix() { // Retrieve the matrix from the array
 }
 
 //This does the actual creation of the scene
-function draw(gl, u_ModelMatrix, u_NormalMatrix, u_isLighting) {
+function draw() {
+	
+	// Set the light color (white)
+	gl.uniform3f(u_LightColor1, 1, 1, 1);
+	// Set the light direction (in the world coordinate)
+	var lightDirection1 = new Vector3([0.7, 0.6, -0.2]);
+	gl.uniform3fv(u_LightDirection1, lightDirection1.elements);
+	
+	gl.uniform3f(u_LightColor2, 1, 1, 1);
+	var lightDirection2 = new Vector3([-0.2, -0.4, 0.7]);
+	gl.uniform3fv(u_LightDirection2, lightDirection2.elements);
+	
+	gl.uniform3f(u_LightPosition, -1.4, 1.7, 5)
+	gl.uniform3f(u_LightColor3, 1, 0.95, 0.9)
+	
 	// Clear color and depth buffer
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	// Pass the model matrix to the uniform variable
 	gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-	gl.uniform1i(u_isLighting, true); // Will apply lighting
 
 	// Set the vertex coordinates and color (for the cube)
 	var n = initVertexBuffers(gl);
@@ -275,130 +289,137 @@ function draw(gl, u_ModelMatrix, u_NormalMatrix, u_isLighting) {
 		console.log('Failed to set the vertex information');
 		return;
 	}
-
-	var ctx = {}
-	ctx.gl = gl
-	ctx.u_ModelMatrix = u_ModelMatrix
-	ctx.u_NormalMatrix = u_NormalMatrix
-	ctx.n = n
 	
 	updateViewMatrix(gl)
 
-	modelWalls(ctx, 0, 0, 0)
-	modelDesksAndChairs(ctx, 0, 0, 0)
+	modelWalls(n, 0, 0, 0)
+	modelDesksAndChairs(n, 0, 0, 0)
 }
 
-function modelWalls(ctx, x, y, z){
-	addCube(ctx, x, y+2, z+10, 25, 7, 0.2, color_wall) //whiteboard wall
-	addCube(ctx, x, y+3, z+9.9, 7, 4, 0.2, color_white) //whiteboard
-	addCube(ctx, x, y+1, z+9.7, 7.2, 0.2, 0.6, color_dgrey) //bottom border
-	addCube(ctx, x-3.5, y+3, z+9.85, 0.1, 4.2, 0.2, color_dgrey) //right border
-	addCube(ctx, x+3.5, y+3, z+9.85, 0.1, 4.2, 0.2, color_dgrey) //left border
-	addCube(ctx, x, y+5.05, z+9.85, 7, 0.1, 0.2, color_dgrey) //top border
+function modelWalls(n, x, y, z){
+	addCube(n, x, y+2, z+10, 25, 7, 0.2, color_wall) //whiteboard wall
+	addCube(n, x, y+3, z+9.9, 7, 4, 0.2, color_white) //whiteboard
+	addCube(n, x, y+1, z+9.7, 7.2, 0.2, 0.6, color_dgrey) //bottom border
+	addCube(n, x-3.5, y+3, z+9.85, 0.1, 4.2, 0.2, color_dgrey) //right border
+	addCube(n, x+3.5, y+3, z+9.85, 0.1, 4.2, 0.2, color_dgrey) //left border
+	addCube(n, x, y+5.05, z+9.85, 7, 0.1, 0.2, color_dgrey) //top border
 	
-	addCube(ctx, x-11.99, y+1, z+6, 0.2, 6, 2, color_wood) //door wall 1
-	addCube(ctx, x-12, y+2, z-2, 0.2, 7, 25, color_wall) //door wall 2
+	addCube(n, x-11.99, y+1, z+6, 0.2, 6, 2, color_wood) //door wall 1
+	addCube(n, x-12, y+2, z-2, 0.2, 7, 25, color_wall) //door wall 2
 	
-	addCube(ctx, x-11.9, y+1, z+7, 0.2, 6, 0.2, color_lwood) //door outer 1
-	addCube(ctx, x-11.9, y+4, z+6, 0.2, 0.2, 2.4, color_lwood) //door outer 2
-	addCube(ctx, x-11.9, y+1, z+5, 0.2, 6, 0.2, color_lwood) //door outer 3
-	addCube(ctx, x-11.8, y+1, z+5.35, 0.15, 0.15, 0.15, color_lgrey) //door handle
+	addCube(n, x-11.9, y+1, z+7, 0.2, 6, 0.2, color_lwood) //door outer 1
+	addCube(n, x-11.9, y+4, z+6, 0.2, 0.2, 2.4, color_lwood) //door outer 2
+	addCube(n, x-11.9, y+1, z+5, 0.2, 6, 0.2, color_lwood) //door outer 3
+	addCube(n, x-11.8, y+1, z+5.35, 0.15, 0.15, 0.15, color_lgrey) //door handle
 	
-	addCube(ctx, x+12, y+2, z-10, 0.2, 7, 10, color_wall) //window wall 1
-	addCube(ctx, x+12, y+2, z+6, 0.2, 7, 10, color_wall) //window wall 2
-	addCube(ctx, x+12, y-1, z-2, 0.2, 2, 7, color_wall) //window wall 3
-	addCube(ctx, x+12, y+6, z-2, 0.2, 2, 7, color_wall) //window wall 4
-	addCube(ctx, x+12, y+2.5, z-5, 0.4, 5, 0.4, color_wood) //window outer 1
-	addCube(ctx, x+12, y+2.5, z+1, 0.4, 5, 0.4, color_wood) //window outer 2
-	addCube(ctx, x+12, y+0.2, z-2, 0.4, 0.4, 6, color_wood) //window outer 3
-	addCube(ctx, x+12, y+4.8, z-2, 0.4, 0.4, 6, color_wood) //window outer 4
-	addCube(ctx, x+12, y+2.5, z-2, 0.2, 0.2, 6, color_wood) //window inner 1
-	addCube(ctx, x+12, y+2.5, z-2, 0.2, 5, 0.2, color_wood) //window inner 2
-	addCube(ctx, x+12, y+2.5, z-2, 0.1, 5, 6, color_glass) //window glass
+	addCube(n, x+12, y+2, z-10, 0.2, 7, 10, color_wall) //window wall 1
+	addCube(n, x+12, y+2, z+6, 0.2, 7, 10, color_wall) //window wall 2
+	addCube(n, x+12, y-1, z-2, 0.2, 2, 7, color_wall) //window wall 3
+	addCube(n, x+12, y+6, z-2, 0.2, 2, 7, color_wall) //window wall 4
+	addCube(n, x+12, y+2.5, z-5, 0.4, 5, 0.4, color_wood) //window outer 1
+	addCube(n, x+12, y+2.5, z+1, 0.4, 5, 0.4, color_wood) //window outer 2
+	addCube(n, x+12, y+0.2, z-2, 0.4, 0.4, 6, color_wood) //window outer 3
+	addCube(n, x+12, y+4.8, z-2, 0.4, 0.4, 6, color_wood) //window outer 4
+	addCube(n, x+12, y+2.5, z-2, 0.2, 0.2, 6, color_wood) //window inner 1
+	addCube(n, x+12, y+2.5, z-2, 0.2, 5, 0.2, color_wood) //window inner 2
+	addCube(n, x+12, y+2.5, z-2, 0.1, 5, 6, color_glass) //window glass
 	
-	addCube(ctx, x, y+2, z-14, 25, 7, 0.2, color_wall) //back wall
-	addCube(ctx, x, y+5.6, z-2, 25, 0.2, 25, color_ceiling) //ceiling
-	addCube(ctx, x, y-1.3, z-2, 25, 0.2, 25, color_floor) //floor
+	addCube(n, x, y+2, z-14, 25, 7, 0.2, color_wall) //back wall
+	addCube(n, x, y+5.6, z-2, 25, 0.2, 25, color_ceiling) //ceiling
+	addCube(n, x, y-1.3, z-2, 25, 0.2, 25, color_floor) //floor
 }
 
-function modelDesksAndChairs(ctx, x, y, z){
-	modelTeacherChairDesk(ctx, x, y, z+6)
-	modelChairDeskRows(ctx, x, y-1, z-6)
+function modelDesksAndChairs(n, x, y, z){
+	var liftAmount = document.getElementById('lift').value / 100
+	modelTeacherChairDesk(n, x, y + liftAmount, z+6)
+	modelChairDeskRows(n, x, y-1 + liftAmount, z-6)
 }
 
-function modelTeacherChairDesk(ctx, x, y, z){
-	modelTeacherChair(ctx, x, y, z)
-	modelTeacherDesk(ctx, x, y, z-1)
+function modelTeacherChairDesk(n, x, y, z){
+	modelTeacherChair(n, x, y, z+1.5)
+	modelTeacherDesk(n, x, y, z-1.5)
 }
 
-function modelTeacherChair(ctx, x, y, z) {
-	addCube(ctx, x, y, z, 1, 0.2, 1, color_lgrey) //seat
-	addCube(ctx, x, y+1, z+0.4, 1, 1.3, 0.2, color_lgrey) //back
-	addCube(ctx, x, y+0.4, z+0.5, 0.2, 0.9, 0.15, color_dgrey) //connector
-	addCube(ctx, x, y-0.5, z, 0.2, 0.9, 0.2, color_dgrey) //wheel connector
-	addCubeWithRotate(ctx, x, y-0.95, z, 1.6, 0.2, 0.2, 0, 45, 0, color_dgrey) //one diagonal
-	addCubeWithRotate(ctx, x, y-0.95, z, 1.6, 0.2, 0.2, 0, -45, 0, color_dgrey) //one diagonal
-	addCubeWithRotate(ctx, x+0.5, y-1.1, z+0.5, 0.15, 0.15, 0.15, 0, 45, 0, color_black) //one wheel
-	addCubeWithRotate(ctx, x-0.5, y-1.1, z+0.5, 0.15, 0.15, 0.15, 0, 45, 0, color_black) //one wheel
-	addCubeWithRotate(ctx, x+0.5, y-1.1, z-0.5, 0.15, 0.15, 0.15, 0, -45, 0, color_black) //one wheel
-	addCubeWithRotate(ctx, x-0.5, y-1.1, z-0.5, 0.15, 0.15, 0.15, 0, -45, 0, color_black) //one wheel
+function modelTeacherChair(n, x, y, z) {
+	addCube(n, x, y, z, 1, 0.2, 1, color_lgrey) //seat
+	addCube(n, x, y+1, z+0.4, 1, 1.3, 0.2, color_lgrey) //back
+	addCube(n, x, y+0.4, z+0.5, 0.2, 0.9, 0.15, color_dgrey) //connector
+	addCube(n, x, y-0.5, z, 0.2, 0.9, 0.2, color_dgrey) //wheel connector
+	addCubeWithRotate(n, x, y-0.95, z, 1.6, 0.2, 0.2, 0, 45, 0, color_dgrey) //one diagonal
+	addCubeWithRotate(n, x, y-0.95, z, 1.6, 0.2, 0.2, 0, -45, 0, color_dgrey) //one diagonal
+	addCubeWithRotate(n, x+0.5, y-1.1, z+0.5, 0.15, 0.15, 0.15, 0, 45, 0, color_black) //one wheel
+	addCubeWithRotate(n, x-0.5, y-1.1, z+0.5, 0.15, 0.15, 0.15, 0, 45, 0, color_black) //one wheel
+	addCubeWithRotate(n, x+0.5, y-1.1, z-0.5, 0.15, 0.15, 0.15, 0, -45, 0, color_black) //one wheel
+	addCubeWithRotate(n, x-0.5, y-1.1, z-0.5, 0.15, 0.15, 0.15, 0, -45, 0, color_black) //one wheel
 }
 
-function modelTeacherDesk(ctx, x, y, z) {
-	addCube(ctx, x, y + 0.75, z, 4, 0.2, 2, color_wood) //chair back
-	addCube(ctx, x + 1.4, y-0.2, z, 1, 1.9, 1.8, color_wood) //left side
-	addCube(ctx, x - 1.4, y-0.2, z, 1, 1.9, 1.8, color_wood) //right side
+function modelTeacherDesk(n, x, y, z) {
+	addCube(n, x, y + 0.75, z, 4, 0.2, 2, color_wood) //chair back
+	addCube(n, x + 1.4, y-0.2, z, 1, 1.9, 1.8, color_wood) //left side
+	addCube(n, x - 1.4, y-0.2, z, 1, 1.9, 1.8, color_wood) //right side
+	
+	modelLamp(n, x-1.4, y, z)
 }
 
-function modelChairDeskRows(ctx, x, y, z) {
+function modelLamp(n, x, y, z){
+	addCubeWithRotate(n, x, y+0.875, z, 0.5, 0.05, 0.05, 0, 45, 0, color_gold) //one diagonal
+	addCubeWithRotate(n, x, y+0.875, z, 0.5, 0.05, 0.05, 0, -45, 0, color_gold) //one diagonal
+	addCubeWithRotate(n, x, y+1.15, z, 0.1, 0.5, 0.1, 0, -45, 0, color_gold) //upright
+	addCubeWithRotate(n, x, y+1.7, z, 0.4, 0.6, 0.4, 0, lampRotation, 0, color_gold) //top
+	if(document.getElementById('spin').checked){
+		lampRotation += 10
+	}
+}
+
+function modelChairDeskRows(n, x, y, z) {
 	var arr = [-5.5, -2.75, 0, 2.75, 5.5]
 	arr.forEach(function(element) {
-		modelChairDeskRow(ctx, x, y, z + element)
+		modelChairDeskRow(n, x, y, z + element)
 	})
 }
 
-function modelChairDeskRow(ctx, x, y, z) {
+function modelChairDeskRow(n, x, y, z) {
 	var arr = [-9.5, -7, -4.5, -2, 2, 4.5, 7, 9.5]
 	arr.forEach(function(element) {
-		modelChairDesk(ctx, x + element, y, z)
+		modelChairDesk(n, x + element, y, z)
 	})
 }
 
-function modelChairDesk(ctx, x, y, z) {
-	modelChair(ctx, x, y, z)
-	modelDesk(ctx, x + 1, y+0.1, z + 1)
+function modelChairDesk(n, x, y, z) {
+	modelChair(n, x, y, z)
+	modelDesk(n, x + 1, y+0.1, z + 1)
 }
 
-function modelDesk(ctx, x, y, z) {
-	addCube(ctx, x - 0.625, y + 1, z, 2, 0.1, 1, color_lwood) //desk top
-	addCube(ctx, x + 0.2, y + 0.275, z + 0.375, 0.1, 1.4, 0.1, color_lwood) //front right leg
-	addCube(ctx, x - 1.45, y + 0.275, z + 0.375, 0.1, 1.4, 0.1, color_lwood) //front left leg
-	addCube(ctx, x + 0.2, y + 0.275, z - 0.375, 0.1, 1.4, 0.1, color_lwood) //back right leg
-	addCube(ctx, x - 1.45, y + 0.275, z - 0.375, 0.1, 1.4, 0.1, color_lwood) //back left leg
+function modelDesk(n, x, y, z) {
+	addCube(n, x - 0.625, y + 1, z, 2, 0.1, 1, color_lwood) //desk top
+	addCube(n, x + 0.2, y + 0.275, z + 0.375, 0.1, 1.4, 0.1, color_lwood) //front right leg
+	addCube(n, x - 1.45, y + 0.275, z + 0.375, 0.1, 1.4, 0.1, color_lwood) //front left leg
+	addCube(n, x + 0.2, y + 0.275, z - 0.375, 0.1, 1.4, 0.1, color_lwood) //back right leg
+	addCube(n, x - 1.45, y + 0.275, z - 0.375, 0.1, 1.4, 0.1, color_lwood) //back left leg
 }
 
-function modelChair(ctx, x, y, z) {
-	addCube(ctx, x + 0.375, y + 0.5, z + 0.375, 1, 0.25, 1, color_navy) //chair seat
-	addCube(ctx, x + 0.375, y + 1.125, z, 1, 1, 0.25, color_navy) //chair back
-	addCube(ctx, x + 0.75, y, z + 0.75, 0.25, 0.75, 0.25, color_wood) //front right leg
-	addCube(ctx, x, y, z + 0.75, 0.25, 0.75, 0.25, color_wood) //front left leg
-	addCube(ctx, x + 0.75, y, z, 0.25, 0.75, 0.25, color_wood) //back right leg
-	addCube(ctx, x, y, z, 0.25, 0.75, 0.25, color_wood) //back left leg
+function modelChair(n, x, y, z) {
+	addCube(n, x + 0.375, y + 0.5, z + 0.375, 1, 0.25, 1, color_navy) //chair seat
+	addCube(n, x + 0.375, y + 1.125, z, 1, 1, 0.25, color_navy) //chair back
+	addCube(n, x + 0.75, y, z + 0.75, 0.25, 0.75, 0.25, color_wood) //front right leg
+	addCube(n, x, y, z + 0.75, 0.25, 0.75, 0.25, color_wood) //front left leg
+	addCube(n, x + 0.75, y, z, 0.25, 0.75, 0.25, color_wood) //back right leg
+	addCube(n, x, y, z, 0.25, 0.75, 0.25, color_wood) //back left leg
 }
 
-function addCube(ctx, x, y, z, sX, sY, sZ, color){
-	addCubeWithRotate(ctx, x, y, z, sX, sY, sZ, 0, 0, 0, color);
+function addCube(n, x, y, z, sX, sY, sZ, color){
+	addCubeWithRotate(n, x, y, z, sX, sY, sZ, 0, 0, 0, color);
 }
 
-function addCubeWithRotate(ctx, x, y, z, sX, sY, sZ, rX, rY, rZ, color) {
-	setColors(ctx.gl, color)
+function addCubeWithRotate(n, x, y, z, sX, sY, sZ, rX, rY, rZ, color) {
+	setColors(gl, color)
 	pushMatrix(modelMatrix);
 	modelMatrix.translate(x, y, z); // Translation
 	modelMatrix.rotate(rX, 1, 0, 0); //Rotate x
 	modelMatrix.rotate(rY, 0, 1, 0); //Rotate y
 	modelMatrix.rotate(rZ, 0, 0, 1); //Rotate z
 	modelMatrix.scale(sX, sY, sZ); // Scale
-	drawbox(ctx.gl, ctx.u_ModelMatrix, ctx.u_NormalMatrix, ctx.n);
+	drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
 	modelMatrix = popMatrix();
 }
 
